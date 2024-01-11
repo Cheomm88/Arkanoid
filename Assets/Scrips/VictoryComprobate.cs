6423d73acc3f6ea681294a0d58c6a9169862ff5e
Cheomm88 <chopids@gmail.com>
2024-01-11 12:36:27 +0000

    Add victory screen

--- Assets/Scrips/VictoryComprobate.cs
+++ Assets/Scrips/VictoryComprobate.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VictoryComprobate : MonoBehaviour
 {
+    public GameObject victoryScreen;
     // Update is called once per frame
     void Update()
     {
-        if (transform.childCount == 0)
+        if (transform.childCount == 0 && victoryScreen.activeSelf == false)
         {
-            Debug.Log("Victoria");
+            victoryScreen.SetActive(true);
         }
+    }
+
+    public void ResetGame()
+    {
+        Debug.Log("Reiniciar partida");
+        SceneManager.LoadScene(0);
     }
 }
